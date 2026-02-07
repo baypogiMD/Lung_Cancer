1de0d1c209adb97ba58359a2bd13a4b3162a28f2
baypogiMD <49798713+baypogiMD@users.noreply.github.com>
2026-02-07 10:42:39 +0800

    Create 01_schema.sql

---
+++ sql/01_schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE lung_cancer (
+    id INTEGER PRIMARY KEY,
+    gender TEXT,
+    age INTEGER,
+    smoking INTEGER,
+    yellow_fingers INTEGER,
+    anxiety INTEGER,
+    peer_pressure INTEGER,
+    chronic_disease INTEGER,
+    fatigue INTEGER,
+    allergy INTEGER,
+    wheezing INTEGER,
+    alcohol_consumption INTEGER,
+    coughing INTEGER,
+    shortness_of_breath INTEGER,
+    swallowing_difficulty INTEGER,
+    chest_pain INTEGER,
+    lung_cancer INTEGER
+);
